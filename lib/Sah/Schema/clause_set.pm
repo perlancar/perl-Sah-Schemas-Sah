1525797f513a311a31cec83989b30712146d2056
perlancar (@pc-home) <perlancar@gmail.com>
2016-07-24 17:50:40 +0700

    Initial commit

---
+++ lib/Sah/Schema/clause_set.pm
@@ -0,0 +1,29 @@
+package Sah::Schema::sah::clause_set;
+
+# DATE
+# VERSION
+
+# incomplete
+
+our $schema = [defhash => {
+    _prop => {
+        # from defhash
+        v => {},
+        defhash_v => {},
+        name => {},
+        summary => {},
+        description => {},
+        tags => {},
+        default_lang => {},
+        x => {},
+
+        # incomplete
+        clause => {
+        },
+        clset => {
+        },
+    },
+}, {}];
+
+1;
+# ABSTRACT: Sah clause set
